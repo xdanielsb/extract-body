10c7cb1f7b1c1b762d3765d3f5319abdefbe8b2b
Daniel <dfsantosbu@unal.edu.co>
2019-11-09 01:56:40 +0100

    Add some comments.

--- logic/extract.py
+++ logic/extract.py
@@ -15,6 +15,12 @@ class ExtractorBody:
     BLUE_IDX       = 1
 
     def __init__( self, **kwargs ):
+        """
+            Contructor, Build and load the objects needed to
+            extract the body.
+                util: Utilitary class
+
+        """
         self.util = Util()
         self.imgReal = self.util.readImage( kwargs["pathReal"] )
         self.imgTemp = self.util.readImage( kwargs["pathTemp"] )
@@ -25,6 +31,10 @@ class ExtractorBody:
         self.mPre = np.dot( self.mCamera, self.mTrans)
 
     def undistort( self, img, dx=0, dy=0):
+        """
+            logic to implement transform:
+            https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html
+        """
         for row in range( self.rows ):
             for col in range( self.cols ):
                 aux = np.array([row, col, 0, 1])
@@ -34,6 +44,10 @@ class ExtractorBody:
                 img[res[self.RED_IDX]+dx][res[self.BLUE_IDX]+dy] = img[row][col]
 
     def getAvgSumRedPixels( self, img):
+        """
+            Return the average of the sum of the red pixel values,
+            given a contourn previously selected.
+        """
         add, npixels =0., 0
         for row in range( self.rows ):
             for col in range( self.cols ):
@@ -44,12 +58,26 @@ class ExtractorBody:
         return avg
 
     def cleanValuesNotInSelectedRegion( self):
+        """
+            Set white color in all pixels that are not inside a
+            previously selected region
+        """
         for row in range( self.rows ):
             for col in range( self.cols ):
                 if(sum(self.imgTemp[row][col]) == self.SUM_RGB_WHITE): continue
                 self.imgReal[row][col] = self.util.WHITE
 
     def selectValidBodies( self, contours ):
+        """
+            Function in charge the select potential human bodies,
+            based on the following restrictions
+                1- has a min MIN_AREA_BODY
+                    this should be variable, based on the image, the boddies
+                    could be smaller given the image.
+                2- has a min RED_THRESH:
+                    means that inside the contour there is something hot
+                    then, has a heat source
+        """
         possible_bodies = []
         for i in range( 0, len(contours)):
             area  = cv2.contourArea( contours[i] )
@@ -65,6 +93,11 @@ class ExtractorBody:
         return possible_bodies
 
     def extract( self ):
+        """
+            This a template method, in charge of assemble
+            all parts to extract the body, could be understood
+            as the main part of the algorithm
+        """
         self.undistort(self.imgReal)
         self.undistort(self.imgTemp,15, -10)
         th = self.util.applyThresh(self.imgTemp)
--- logic/util.py
+++ logic/util.py
@@ -1,3 +1,7 @@
+"""
+    Class in charge of provide some Utilitary functions.
+"""
+
 import cv2
 
 class Util:
@@ -7,19 +11,36 @@ class Util:
         pass
 
     def readImage(self, path, tt=1):
+        """
+            Read an image given a path
+            tt, mean if you want to read in {color, black}
+        """
         return cv2.imread( path, tt)
 
     def showImage(self, img):
+        """
+            Display an image in the screen, until
+            the user press a key.
+        """
         cv2.namedWindow(self.NAME_WINDOW,cv2.WINDOW_NORMAL)
         cv2.resizeWindow(self.NAME_WINDOW, 300, 700)
         cv2.imshow(self.NAME_WINDOW , img)
         cv2.waitKey(0)
 
     def togray( self, img ):
+        """
+            Change an image to gray scale
+        """
+        if( len(img.shape) == 2): return img
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         return gray
 
     def getHistogram( self, img):
+        """
+            Function that help me to get the histogram
+            Phantom code, at the end, useful to spot some
+            futures at the begining, distribution of color
+        """
         bins = 256
         range_scale = [0,254]
         nivel_transparencia = 0.5
@@ -28,14 +49,25 @@ class Util:
         plt.show()
 
     def applyCanny( self, img):
+        """
+            Function to apply canny filter
+        """
         img = togray( img )
         res = cv2.Canny( img ,480,500)
         return res
 
     def fillContour( self, img, contours, i):
+        """
+            Given a contour, an an image fill that contour with white color
+            in the image
+        """
         cv2.drawContours(img, contours, i , self.WHITE, -1)
 
     def removeBlue(self, img):
+        """
+            Function that helps to remove a certain gama of
+            colors. Phantom code now.
+        """
         lower = np.array([0,30,255])
         upper = np.array([255,255,255])
         mask = cv2.inRange(img, lower, upper)
@@ -43,6 +75,9 @@ class Util:
         return res
 
     def applyThresh(self, im, mi=100, ma=255):
+        """
+            Function that helps to apply a thresh.
+        """
         if( len(im.shape) == 3):
             _, thr= cv2.threshold(self.togray( im ) ,mi,ma,cv2.THRESH_BINARY)
         else:
@@ -50,6 +85,9 @@ class Util:
         return thr
 
     def getContours( self,img ):
+        """
+            Function that helps to get the contours
+        """
         if( len(img.shape) == 3):
             (_, contours, _) = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         else:
